b60770761b6e677c19f04b6037a4bac5e604e985
Quang Kien Vo <quang.vo31@gmail.com>
2018-02-19 13:05:25 -0800

    Update appendIframe.js

--- appendIframe.js
+++ appendIframe.js
@@ -2,7 +2,7 @@ function createIframe() {
   var iframeContainer = document.createElement("IFRAME");
   iframeContainer.setAttribute('src', 'https://squareup.com/store/superinflated-3');
   iframeContainer.setAttribute('style', 'position:absolute; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:2;')
-  var bodyContainer = document.getElementsByTagName("BODY")[0];
+  var bodyContainer = document.getElementsByTagName("HEADER")[0];
   bodyContainer.appendChild(iframeContainer);
 }
 
